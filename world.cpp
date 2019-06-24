0e4e7a5f3329ee03ee4e632f46391e333de54740
flightcoded <37447279+flightcoded@users.noreply.github.com>
2019-06-24 19:17:25 +0300

    partially refined code

--- world.cpp
+++ world.cpp
@@ -1,52 +1,79 @@
 
 #include "world.h"
 
-world::world(double seed):
-	low("resources/textures/low.png"),
-	sea("resources/textures/high.png"),
-	ss(100, 50),
-	ll(100, 50)
+world::world(int worldsize, float seed_):
+
+	lowtile("resources/textures/low.png"),
+	seatile("resources/textures/sea.png"),
+	sea(100, 50),
+	low(100, 50),
+	high(100, 50),
+	higher(100, 50),
+	seed(seed_)
 {
 
-	int grid[400][400];
+	sea.offset = 5;
+	high.offset = -10;
+	higher.offset = -5;
+
+	unsigned int s_count = 0;
+	unsigned int l_count = 0;
+	unsigned int h_count = 0;
+	unsigned int h2_count = 0;
+
+	int** grid = new int* [worldsize];
+	for(int i = 0; i < worldsize; ++i) grid[i] = new int[worldsize];
 
 	double xoff = 0.0;
 	double yoff = 0.0;
-	unsigned int s_count = 0;
-	unsigned int l_count = 0;
-	for(int i = 0; i < 400; i++){
+	for(int i = 0; i < worldsize; i++){
 
 		yoff = 0.0;
-		for(int j = 0; j < 400; j++){
+		for(int j = 0; j < worldsize; j++){
 
-			grid[i][j] = abs((int)(stb_perlin_noise3(xoff, yoff, seed, 0, 0, 0) * 4));
-
-			if(grid[i][j] == 1){
-
-				s_count++;
-			}
+			grid[i][j] = (int) map(stb_perlin_noise3(xoff, yoff, seed, 0, 0, 0), 0, 1, 2, 5);
+			if(grid[i][j] == 1) s_count++;
+			else if(grid[i][j] == 4) h2_count++;
+			else if(grid[i][j] == 3) h_count++;
 			else l_count++;
-
-			yoff += 0.005;
+			yoff += 0.02;
 		}
-		xoff += 0.005;
+		xoff += 0.02;
 	}
 
 	glm::vec2* ds = new glm::vec2[s_count];
 	glm::vec2* dl = new glm::vec2[l_count];
+	glm::vec2* dh = new glm::vec2[h_count];
+	glm::vec2* dh2 = new glm::vec2[h2_count];
 
 	int a_ = 0;
 	int b_ = 0;
+	int c_ = 0;
+	int d_ = 0;
 
-	for(int i = 0; i < 400; i++){
+	for(int i = 0; i < worldsize; i++){
 
-		for(int j = 0; j < 400; j++){
+		for(int j = 0; j < worldsize; j++){
 		
 			if(grid[i][j] == 1){
 				if(a_ < s_count){
 
-					ds[a_] = ctoi(glm::vec2(i * 50, j * 50));
+					ds[a_] = glm::vec2(ctoi(glm::vec2(i * 50, j * 50)).x, ctoi(glm::vec2(i * 50, j * 50)).y + sea.offset);
 					a_++;
+				}
+			}
+			else if(grid[i][j] == 3){
+				if(c_ < h_count){
+
+					dh[c_] = glm::vec2(ctoi(glm::vec2(i * 50, j * 50)).x, ctoi(glm::vec2(i * 50, j * 50)).y + high.offset);
+					c_++;
+				}
+			}
+			else if(grid[i][j] == 4){
+				if(d_ < h2_count){
+
+					dh2[d_] = glm::vec2(ctoi(glm::vec2(i * 50, j * 50)).x, ctoi(glm::vec2(i * 50, j * 50)).y + higher.offset);
+					d_++;
 				}
 			}
 			else{
@@ -59,30 +86,19 @@ world::world(double seed):
 		}
 	}
 
-	ss.init(s_count, ds);
-	ll.init(l_count, dl);
+	sea.init(s_count, ds);
+	low.init(l_count, dl);
+	high.init(h_count, dh);
+	higher.init(h2_count, dh2);
 }
 
 void world::display(){
 
-	low.bind();
-	ll.display();
-	sea.bind();
-	ss.display();
-}
+	seatile.bind();
+	sea.display();
 
-glm::vec2 world::ctoi(glm::vec2 p){
-
-	glm::vec2 i = p;
-	i.x -= i.y;
-	i.y = (p.x + p.y) / 2;
-	return i;
-}
-
-glm::vec2 world::itoc(glm::vec2 p){
-
-	glm::vec2 i = p;
-	i.x = (p.x + 2 * p.y) / 2;
-	i.y = (2 * p.y - p.x) / 2;
-	return i;
+	lowtile.bind();
+	low.display();
+	high.display();
+	higher.display();
 }
