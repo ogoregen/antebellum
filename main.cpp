3719db9ad6fed843322ffd74f7dd4126ac7538b5
flightcoded <37447279+flightcoded@users.noreply.github.com>
2019-06-24 19:17:03 +0300

    partially refined code

--- main.cpp
+++ main.cpp
@@ -1,104 +1,139 @@
 
+#include <GL/glew.h>
+#include <GLFW/glfw3.h>
+
+#include <iostream>
+#include <fstream>
+#include <string>
+#include <sstream>
+
+#include "glm/glm.hpp"
+#include "glm/gtc/matrix_transform.hpp"
+#include "stb_image/stb_image.h"
+#include "imgui/imgui.h"
+#include <imgui\imgui_impl_glfw_gl3.h>
+
+#include "graphics/shader.h"
+#include "graphics/texture.h"
+
+#include "graphics/thing.h"
 #include "world.h"
 
-world::world(int worldsize, float seed_):
 
-	lowtile("resources/textures/low.png"),
-	seatile("resources/textures/sea.png"),
-	sea(100, 50),
-	low(100, 50),
-	high(100, 50),
-	higher(100, 50),
-	seed(seed_)
-{
+int irr1, irr2, displaywidth, displayheight, width, height;
+double mouseX, mouseY;
+glm::vec2 translation(0, 0);
+GLFWwindow* window;
+float zoom = 1;
 
-	sea.offset = 5;
-	high.offset = -10;
-	higher.offset = -5;
+void onscroll(GLFWwindow* window, double xoffset, double yoffset){
 
-	unsigned int s_count = 0;
-	unsigned int l_count = 0;
-	unsigned int h_count = 0;
-	unsigned int h2_count = 0;
+	if(yoffset > 0 && zoom < 1){
 
-	int** grid = new int* [worldsize];
-	for(int i = 0; i < worldsize; ++i) grid[i] = new int[worldsize];
-
-	double xoff = 0.0;
-	double yoff = 0.0;
-	for(int i = 0; i < worldsize; i++){
-
-		yoff = 0.0;
-		for(int j = 0; j < worldsize; j++){
-
-			grid[i][j] = (int) map(stb_perlin_noise3(xoff, yoff, seed, 0, 0, 0), 0, 1, 2, 5);
-			if(grid[i][j] == 1) s_count++;
-			else if(grid[i][j] == 4) h2_count++;
-			else if(grid[i][j] == 3) h_count++;
-			else l_count++;
-			yoff += 0.02;
-		}
-		xoff += 0.02;
+		zoom += 0.1;
+		translation.y += height / 2 - mouseY;
+		translation.x += width / 2 - mouseX;
 	}
+	else if(yoffset < 0 && zoom > 0.5){
 
-	glm::vec2* ds = new glm::vec2[s_count];
-	glm::vec2* dl = new glm::vec2[l_count];
-	glm::vec2* dh = new glm::vec2[h_count];
-	glm::vec2* dh2 = new glm::vec2[h2_count];
-
-	int a_ = 0;
-	int b_ = 0;
-	int c_ = 0;
-	int d_ = 0;
-
-	for(int i = 0; i < worldsize; i++){
-
-		for(int j = 0; j < worldsize; j++){
-		
-			if(grid[i][j] == 1){
-				if(a_ < s_count){
-
-					ds[a_] = glm::vec2(ctoi(glm::vec2(i * 50, j * 50)).x, ctoi(glm::vec2(i * 50, j * 50)).y + sea.offset);
-					a_++;
-				}
-			}
-			else if(grid[i][j] == 3){
-				if(c_ < h_count){
-
-					dh[c_] = glm::vec2(ctoi(glm::vec2(i * 50, j * 50)).x, ctoi(glm::vec2(i * 50, j * 50)).y + high.offset);
-					c_++;
-				}
-			}
-			else if(grid[i][j] == 4){
-				if(d_ < h2_count){
-
-					dh2[d_] = glm::vec2(ctoi(glm::vec2(i * 50, j * 50)).x, ctoi(glm::vec2(i * 50, j * 50)).y + higher.offset);
-					d_++;
-				}
-			}
-			else{
-				if(b_ < l_count){
-
-					dl[b_] = ctoi(glm::vec2(i * 50, j * 50));
-					b_++;
-				}
-			}
-		}
+		zoom -= 0.1;
+		translation.y -= height / 2 - mouseY;
+		translation.x -= width / 2 - mouseX;
 	}
-
-	sea.init(s_count, ds);
-	low.init(l_count, dl);
-	high.init(h_count, dh);
-	higher.init(h2_count, dh2);
 }
 
-void world::display(){
+static void view(){
 
-	seatile.bind();
-	sea.display();
+	glfwGetCursorPos(window, &mouseX, &mouseY);
+	if(mouseX < 15.0f) translation.x += 15.0f;
+	else if(mouseX > width - (double)15.0f) translation.x -= 15.0f;
+	if(mouseY < 15.0f) translation.y += 15.0f;
+	else if(mouseY > height - (double)15.0f) translation.y -= 15.0f;
+}
 
-	lowtile.bind();
-	low.display();
-	high.display();
-	higher.display();
+static void hud(){
+
+	ImGui_ImplGlfwGL3_NewFrame();
+	{	
+		ImGui::SliderFloat("scale", &zoom, 0.5f, 1.0f);
+		ImGui::Text("average %.3f ms/frame (%.1f FPS)", 1000.0f / ImGui::GetIO().Framerate, ImGui::GetIO().Framerate);
+	}
+	ImGui::Render();
+	ImGui_ImplGlfwGL3_RenderDrawData(ImGui::GetDrawData());
+}
+
+static int initialize(){
+
+	if(!glfwInit()) return -1;
+
+	glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
+	glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
+	glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+	glfwGetMonitorWorkarea(glfwGetPrimaryMonitor(), &irr1, &irr2, &displaywidth, &displayheight);
+	width = displaywidth;
+	height = displayheight;
+	window = glfwCreateWindow(width, height, "lite", glfwGetPrimaryMonitor(), NULL);
+	if(!window){
+
+		glfwTerminate();
+		return -1;
+	}
+	glfwMakeContextCurrent(window);
+	glfwSwapInterval(1);
+	if(glewInit() != GLEW_OK) return -1;
+	std::cout << "Be positive, please. " << "Using " << glGetString(GL_VERSION) << std::endl;
+	glEnable(GL_BLEND);
+	glBlendFunc(GL_SRC_ALPHA, GL_ONE_MINUS_SRC_ALPHA);
+
+	ImGui::CreateContext();
+	ImGui_ImplGlfwGL3_Init(window, true);
+	ImGui::StyleColorsDark();
+
+	glClearColor(0.0f, 0.0f, 0.0f, 1.0f);
+
+	glfwSetScrollCallback(window, onscroll);
+}
+
+int main(void){
+
+	initialize();
+	glm::mat4 proj = glm::ortho(0.0f, (float)width, (float)height, 0.0f);
+	world map(50, 572212469);
+	texture bluetile("resources/textures/selector.png");
+	thing selector(100, 50);
+	shader basic("resources/shaders/basic_vs.shader", "resources/shaders/basic_fs.shader");
+	basic.bind();
+
+	while(!glfwWindowShouldClose(window)){
+
+		if(glfwGetKey(window, GLFW_KEY_ESCAPE) == GLFW_PRESS) glfwSetWindowShouldClose(window, true);
+		glClear(GL_COLOR_BUFFER_BIT);
+
+		glm::vec2 cursor(mouseX - translation.x, mouseY - translation.y);
+		cursor.x = (int)(cursor.x / 100);
+		cursor.y = (int)(cursor.y / 100);
+
+		view();
+
+		glm::mat4 scale = glm::scale(glm::mat4(1.0f), glm::vec3(zoom, zoom, zoom));
+		glm::mat4 view = glm::translate(glm::mat4(1.0f), glm::vec3(translation.x, translation.y, 0));
+
+		basic.setUmat4f("mvp", proj * view * scale);
+		map.display();
+
+		glm::mat4 model = glm::translate(glm::mat4(1.0f), glm::vec3(cursor.x * 100, cursor.y * 100, 0));
+		basic.setUmat4f("mvp", proj * view * scale * model);
+		bluetile.bind();
+		selector.display();
+
+		hud();
+
+		glfwSwapBuffers(window);
+		glfwPollEvents();
+	}
+
+	glfwTerminate();
+	ImGui_ImplGlfwGL3_Shutdown();
+	ImGui::DestroyContext();
+	return 0;
 }
