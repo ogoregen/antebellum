8283658e1e06213831a4a6f4e21990044fbd6322
flightcoded <37447279+flightcoded@users.noreply.github.com>
2019-07-03 21:19:12 +0300

    improved terrain

--- world.h
+++ world.h
@@ -10,17 +10,41 @@
 
 #include "graphics/thing.h"
 #include "graphics/texture.h"
-
 #include "functions.h"
 
 class world{
 
 	private:
-		float seed;
-		texture lowtile, seatile;
-		thing sea, low, high, higher;
+
+		float seed, worldsize;
+		int tilecount = 6;
+		int tilewidth = 100;
+		int tileheight = tilewidth / 2;
+		texture textures[6] = { //in order of drawing
+
+			texture("resources/textures/sea.png"),
+			texture("resources/textures/sea2.png"),
+			texture("resources/textures/sea3.png"),
+
+			texture("resources/textures/low.png"),
+			texture("resources/textures/low2.png"),
+			texture("resources/textures/low3.png")
+		};
+		thing things[6] = {
+
+			thing(tilewidth, textures[0].h / (textures[0].w / tilewidth)),
+			thing(tilewidth, textures[1].h / (textures[1].w / tilewidth)),
+			thing(tilewidth, textures[2].h / (textures[2].w / tilewidth)),
+
+			thing(tilewidth, textures[3].h / (textures[3].w / tilewidth)),
+			thing(tilewidth, textures[4].h / (textures[4].w / tilewidth)),
+			thing(tilewidth, textures[5].h / (textures[5].w / tilewidth))
+		};
 
 	public:
+
+		int **grid, **next;
 		world(int worldsize, float seed_);
+		void generate();
 		void display();
 };
