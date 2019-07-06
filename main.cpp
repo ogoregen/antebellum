1e66c79b29cc08aa2d6d0044e5d51694c7b6889e
flightcoded <37447279+flightcoded@users.noreply.github.com>
2019-07-06 10:01:35 +0300

    minor

--- main.cpp
+++ main.cpp
@@ -1,19 +1,19 @@
 
 #include <iostream>
 #include <vector>
-
+#include <time.h>
 #include <GL/glew.h>
 #include <GLFW/glfw3.h>
 #include "glm/glm.hpp"
 #include "glm/gtc/matrix_transform.hpp"
 #include "imgui/imgui.h"
 #include <imgui/imgui_impl_glfw_gl3.h>
-
 #include "graphics/shader.h"
 #include "graphics/texture.h"
 #include "graphics/thing.h"
 #include "interface/button.h"
 #include "world.h"
+#include "buildings.h"
 
 int irr1, irr2, displaywidth, displayheight, width, height;
 double mouseX, mouseY;
@@ -21,12 +21,14 @@ glm::vec2 translation(0, 0);
 glm::vec2 cursor;
 GLFWwindow* window;
 float zoom = 1;
+bool add = false;
+int selector_ = 0;
 
 void mousepressed(GLFWwindow* window, int button, int action, int mods){
 
 	if(button == GLFW_MOUSE_BUTTON_LEFT && action == GLFW_PRESS){
 
-
+		add = true;
 	}
 }
 
@@ -49,8 +51,8 @@ void view(){
 	glfwGetCursorPos(window, &mouseX, &mouseY);
 	if(mouseX < 15.0f) translation.x += 15.0f;
 	else if(mouseX > width - (double)15.0f) translation.x -= 15.0f;
-	if(mouseY < 15.0f && translation.y) translation.y += 15.0f;
-	else if(mouseY > height - (double)15.0f && translation.y) translation.y -= 15.0f;
+	if(mouseY < 15.0f && translation.y < 0) translation.y += 15.0f;
+	else if(mouseY > height - (double)15.0f) translation.y -= 15.0f;
 }
 
 void hud(){
@@ -58,6 +60,7 @@ void hud(){
 	ImGui_ImplGlfwGL3_NewFrame();
 	{	
 		ImGui::Text("average %.3f ms/frame (%.1f FPS)", 1000.0f / ImGui::GetIO().Framerate, ImGui::GetIO().Framerate);
+		ImGui::SliderInt("building selector", &selector_, 0, 4);
 	}
 	ImGui::Render();
 	ImGui_ImplGlfwGL3_RenderDrawData(ImGui::GetDrawData());
@@ -72,7 +75,7 @@ int initialize(){
 	glfwGetMonitorWorkarea(glfwGetPrimaryMonitor(), &irr1, &irr2, &displaywidth, &displayheight);
 	width = displaywidth;
 	height = displayheight;
-	window = glfwCreateWindow(width, height, "lite", glfwGetPrimaryMonitor(), NULL);
+	window = glfwCreateWindow(width, height, "lite", NULL, NULL);
 	if(!window){
 
 		glfwTerminate();
@@ -81,7 +84,7 @@ int initialize(){
 	glfwMakeContextCurrent(window);
 	glfwSwapInterval(1);
 	if(glewInit() != GLEW_OK) return -1;
-	std::cout << "Be positive, please. " << "Using " << glGetString(GL_VERSION) << std::endl;
+	std::cout << "Be positive, please. " << std::endl << "Using " << glGetString(GL_VERSION) << std::endl;
 	glEnable(GL_BLEND);
 	glBlendFunc(GL_SRC_ALPHA, GL_ONE_MINUS_SRC_ALPHA);
 
@@ -98,15 +101,18 @@ int initialize(){
 int main(void){
 
 	initialize();
+	buildings b;
 	shader basic("resources/shaders/basic_vs.shader", "resources/shaders/basic_fs.shader");
 	basic.bind();
 	glm::mat4 proj = glm::ortho(0.0f, (float)width, (float)height, 0.0f);
-	world map(100, 01);
+	srand(time(NULL));
+	world map(200, (int)rand());
+	std::cout << "Seed: " << map.seed << std::endl;
 	map.generate();
 
 	texture selectortile("resources/textures/selector.png");
-	thing selector(100, 50);
-	selector.offsetY = +15;
+	thing selector(selectortile.w/2, selectortile.h/2);
+	selector.offsetY = -7.5;
 
 	button buttons[1] = {
 
@@ -122,11 +128,18 @@ int main(void){
 		cursor = itoc(cursor);
 		cursor.x = int(cursor.x / 50);
 		cursor.y = int(cursor.y / 50);
-		cursor.x -= 1;
+		//cursor.x -= 1;
+		cursor.y += 1;
 		cursor = ctoi(cursor);
 		cursor *= 50;
 
 		view();
+
+		if(add){
+
+			b.add(cursor.x / 50, cursor.y / 50, selector_);
+			add = false;
+		}
 
 		glm::mat4 scale = glm::scale(glm::mat4(1.0f), glm::vec3(zoom, zoom, zoom));
 		glm::mat4 view = glm::translate(glm::mat4(1.0f), glm::vec3(translation.x, translation.y, 0));
@@ -138,6 +151,9 @@ int main(void){
 		basic.setUmat4f("mvp", proj * view * scale * model);
 		selectortile.bind();
 		selector.display();
+
+		basic.setUmat4f("mvp", proj * view * scale);
+		b.display(); //display the buildings
 
 		for(int i = 0; i < std::size(buttons); i++){
 
