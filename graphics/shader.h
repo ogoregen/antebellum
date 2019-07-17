c7c01c129c99feace033f117d2c01b622e0ce1dc
flightcoded <37447279+flightcoded@users.noreply.github.com>
2019-07-17 20:34:49 +0300

    added uniform 3d vector

--- graphics/shader.h
+++ graphics/shader.h
@@ -1,4 +1,6 @@
 
+#pragma once
+
 #include <GL/glew.h>
 
 #include <fstream>
@@ -11,10 +13,10 @@
 class shader{
 
 	private:
-		unsigned int ID = 0;
 		std::string loadshader(const std::string& path);
 
 	public:
+		unsigned int ID = 0;
 		shader(const std::string& vs_path, const std::string& fs_path);
 		~shader();
 		void bind();
@@ -22,4 +24,5 @@ class shader{
 		void setU1i(const std::string& name, int value) const;
 		void setU1f(const std::string& name, float value) const;
 		void setUmat4f(const std::string& name, const glm::mat4& value) const;
+		void setUvec3(const std::string& name, const glm::vec3& value) const;
 };
