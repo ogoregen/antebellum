739c504d5f280a65d63862b253c86198a000cae3
flightcoded <37447279+flightcoded@users.noreply.github.com>
2019-07-17 20:34:25 +0300

    added uniform 3d vector

--- graphics/shader.cpp
+++ graphics/shader.cpp
@@ -56,6 +56,11 @@ void shader::setUmat4f(const std::string& name, const glm::mat4& value) const{
 	glUniformMatrix4fv(glGetUniformLocation(ID, name.c_str()), 1, GL_FALSE, &value[0][0]);
 }
 
+void shader::setUvec3(const std::string& name, const glm::vec3& value) const{
+
+	glUniform3f(glGetUniformLocation(ID, name.c_str()), value.x, value.y, value.z);
+}
+
 std::string shader::loadshader(const std::string& path){
 
 	std::ifstream stream(path);
