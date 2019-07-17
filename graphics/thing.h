ddb1dfa1516ef4c80c08327bdc2ee4d71b01838b
flightcoded <37447279+flightcoded@users.noreply.github.com>
2019-07-17 20:33:43 +0300

    updated thing class

--- graphics/thing.h
+++ graphics/thing.h
@@ -1,25 +1,25 @@
 
 #pragma once
-
 #include <GL/glew.h>
 #include <GLFW/glfw3.h>
-
 #include "glm/glm.hpp"
 #include "glm/gtc/matrix_transform.hpp"
+#include "texture.h"
 
 class thing{
 
 	private:
-		unsigned int va, eb, count;
+		texture t;
+		bool ut;
+		unsigned int va, eb, count, vb;
 		float vertices[20];
 		unsigned int indices[6] = {0, 1, 3,	1, 2, 3};
 
 	public:
-		float offset;
-		thing();
-		thing(float w, float h);
-		thing(float w, float h, unsigned int count_, glm::vec2 d[]);
-		void init(unsigned int count_, glm::vec2 d[] = 0);
+		float offsetY;
+		thing(float w, float h, const char* path = "");
+		void instance(unsigned int count_, glm::vec2 d[] = 0);
+		void setOffsetAndSize(float x, float y, float w, float h);
+		void flip();
 		void display();
 };
-
